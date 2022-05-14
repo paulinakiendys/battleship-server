a6e6e7b2e0be8ccf52d773100ece385f0dbd3ce2
Paulina Kiendys <84099095+paulinakiendys@users.noreply.github.com>
2022-05-14 15:43:15 +0200

    Established socket.io connection

--- controllers/socket_controller.js
+++ controllers/socket_controller.js
@@ -6,11 +6,28 @@ const debug = require('debug')('game:socket_controller');
 let io = null; // socket.io server instance
 
 /**
+ * Handle a user disconnecting
+ * 
+ */
+const handleDisconnect = function() {
+	debug(`Client ${this.id} disconnected`)
+}
+
+const handleHello = function() {
+	debug(`Client ${this.id} said hello`)
+}
+
+/**
  * Export controller and attach handlers to events
  *
  */
 module.exports = function(socket, _io) {
 	// save a reference to the socket.io server instance
 	io = _io;
+
+	debug(`Client ${socket.id} connected`)
+
+	// handle user disconnect
+	socket.on('disconnect', handleDisconnect)
 
 }
